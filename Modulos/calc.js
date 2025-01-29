b10195b84ec39e9d436416291e8419940bfb50bc
Gustavo nunes melo <156378462+Gustavonunesmelo@users.noreply.github.com>
2025-01-28 21:40:13 -0300

    Aprendendo sobre modulos.

---
+++ Modulos/calc.js
@@ -0,0 +1,23 @@
+// function sum(a, b) {
+//   return a + b 
+// }
+
+// function multiply(a, b) {
+//   return a * b 
+// }
+
+// export {
+//   sum,
+//   multiply
+// }
+
+export class Calc {
+  sum(a, b) {
+    return a + b
+  }
+
+  multiply(a, b) {
+    return a * b
+  }
+}
+ 
